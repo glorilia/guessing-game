43a733b0bfdd64f122983f662a6ca5098d6293b7
Glorili Alejandro <gloriliale@gmail.com>
2020-07-08 00:59:12 +0000

    Let user play multiple rounds and tracked their best score.

--- game.py
+++ game.py
@@ -13,42 +13,65 @@ print("Oh hi there! What's your name? ")
 # get player name
 player_name = input("(type in your name) ")
 
-# choose target number between MIN and MAX constants
-target = randint(MIN_NUM,MAX_NUM)
-
-#prompt user by name and show range
-print(f"{player_name}, I'm thinking of a number between {MIN_NUM} and {MAX_NUM}.")
-print("Try to guess my number.")
-
-# set up variable for number of guesses
-num_guesses = 0
+# set up variable for lowest score
+lowest_score = 1000
 
 while True:
-    # prompt player for their guess
-    guess = input("Your guess? ")
+    # choose target number between MIN and MAX constants
+    target = randint(MIN_NUM,MAX_NUM)
 
-    # validate that the input is an integer (continue validating till it is)
-    try:
-        guess = int(guess)
-    except ValueError:
-        print("C'mon, you know that's not a number. Try this time.")
-        continue
+    #prompt user by name and show range
+    print(f"{player_name}, I'm thinking of a number between {MIN_NUM} and {MAX_NUM}.")
+    print("Try to guess my number.")
+    print(target)
 
-    # make sure guess is between MIN and MAX
-    if guess < MIN_NUM or guess > MAX_NUM:
-        print(f"Umm, your guess should be between {MIN_NUM} and {MAX_NUM}, remember?")
-        continue
+    # set up variable for number of guesses
+    num_guesses = 1
 
-    if guess != target:
-        #respond with a hint
-        if guess > target:
-            print("Your guess is too high, try again.")
-        if guess < target:
-            print("Your guess is too low, try again")
+    while True:
+        # prompt player for their guess
+        guess = input("Your guess? ")
 
-        #increment number of guesses
-        num_guesses += 1
+        # validate that the input is an integer (continue validating till it is)
+        try:
+            guess = int(guess)
+        except ValueError:
+            print("C'mon, you know that's not a number. Try this time.")
+            continue
 
-    else:
-        print(f"You did it, {player_name}! You guessed the number in {num_guesses} tries!")
+        # make sure guess is between MIN and MAX
+        if guess < MIN_NUM or guess > MAX_NUM:
+            print(f"Umm, your guess should be between {MIN_NUM} and {MAX_NUM}, remember?")
+            continue
+
+        if guess != target:
+            #respond with a hint
+            if guess > target:
+                print("Your guess is too high, try again.")
+            if guess < target:
+                print("Your guess is too low, try again")
+
+            #increment number of guesses
+            num_guesses += 1
+
+        else:
+            print(f"You did it, {player_name}! You guessed the number in {num_guesses} tries!")
+            
+            # display best score
+            if num_guesses < lowest_score:
+                lowest_score = num_guesses
+            print(f"Your best score is {lowest_score}.")
+            break
+
+    # ask user if they'd like to play again
+    print("Wanna play again?")
+    keep_going = input("(Type Y or N) ")
+    while keep_going != "Y" and keep_going != "N":
+        keep_going = input("You must type Y or N! ")
+
+    if keep_going == "N":
         break
+
+
+
+
